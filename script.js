f51de624c9c834a9e783344e4a8769ca0bb02db5
Shirley Wang <shirleywang527@gmail.com>
2019-03-27 22:38:14 +0100

    replace part of jquery with vanilla js

--- script.js
+++ script.js
@@ -15,83 +15,51 @@ $(document).ready(function() {
             $('.hamburger').removeClass('open');
         }
     });
-
-    // smooth scroll
-    $('.about-section').click(function() {
-        $('html, body').animate({
-            scrollTop: $('#about').offset().top
-        }, 800);
-    });
-    $('.skills-section').click(function() {
-        $('html, body').animate({
-            scrollTop: $('#skills').offset().top
-        }, 800);
-    });
-    $('.showcase-section').click(function() {
-        $('html, body').animate({
-            scrollTop: $('#showcase').offset().top
-        }, 800);
-    });
-    $('.contact-section').click(function() {
-        $('html, body').animate({
-            scrollTop: $('#contact').offset().top
-        }, 800);
-    });
-
-    // The arrow appears after scrolling half page
-    $(window).scroll(function() {
-        if($(this).scrollTop() > 500) {
-            $('.scroll-top').removeClass('hidden');
-        }else {
-            $('.scroll-top').addClass('hidden')
-        }
-    });
-
-    // Scroll back to top
-    $('.scroll-top').click(function() {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 800);
-        return false;
-    });
-
-    // highlight follow along in about section
-    const triggers = document.querySelectorAll('em');
-    const highlight = document.createElement('span');
-    highlight.classList.add('highlight');
-    document.body.append(highlight);
-
-    function highlightLinks() {
-        const linkCoords = this.getBoundingClientRect();
-
-        const coords = {
-            width: linkCoords.width,
-            height: linkCoords.height,
-            top: linkCoords.top + window.scrollY,
-            left: linkCoords.left + window.scrollX
-        };
-
-        highlight.style.width = `${coords.width}px`;
-        highlight.style.height = `${coords.height}px`;
-        highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
-    }
-
-    triggers.forEach(em => em.addEventListener('mouseenter', highlightLinks));
 });
 
-// delaying pie chart animation
-// const charts = document.querySelector("#skills");
-// const topOfCharts = charts.offsetTop;
-
-// function fixCharts() {
-//   if (window.scrollY >= topOfCharts) {
-//     // document.body.style.paddingTop = charts.offsetHeight + 'px';
-//     document.body.classList.add("circle_animation");
-//   } else {
-//     // document.body.style.paddingTop = 0;
-//     document.body.classList.remove("circle_animation");
-//   }
-//   console.log(topOfCharts, window.scrollY);
+// function ready(fn) {
+//     if (document.readyState !== 'loading'){
+//         fn();
+//     } else {
+//         document.addEventListener('DOMContentLoaded', fn);
+//     }
 // }
 
-// window.addEventListener("scroll", fixCharts);
+// function fn() {
+//     console.log('doc is ready');
+// }
+
+// fn();
+
+
+// The arrow appears after scrolling half page
+const scrollTop = document.querySelector('.scroll-top');
+
+function fixToTop() {
+    (window.scrollY > 500) ? scrollTop.classList.remove('hidden') : scrollTop.classList.add('hidden');
+}
+
+window.addEventListener('scroll', fixToTop);
+
+// highlight follow along in about section
+const triggers = document.querySelectorAll('em');
+const highlight = document.createElement('span');
+highlight.classList.add('highlight');
+document.body.append(highlight);
+
+function highlightLinks() {
+    const linkCoords = this.getBoundingClientRect();
+
+    const coords = {
+        width: linkCoords.width,
+        height: linkCoords.height,
+        top: linkCoords.top + window.scrollY,
+        left: linkCoords.left + window.scrollX
+    };
+
+    highlight.style.width = `${coords.width}px`;
+    highlight.style.height = `${coords.height}px`;
+    highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
+}
+
+triggers.forEach(em => em.addEventListener('mouseenter', highlightLinks));
